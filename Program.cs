f2516974716242deb35fe99262306b1b26664886
Bashbuilder <iamadelowotan@gmail.com>
2026-06-27 13:23:47 +0100

    feat: added post request and route parameter

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using backend.Dtos;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+const string GetGameEndpointName = "GetGame";
+
 List<GameDto> games = [
   new(
     1,
@@ -27,8 +29,27 @@ List<GameDto> games = [
   ),
 ];
 
-app.MapGet("/", () => "Hello World from a c# application");
+app.MapGet("health", () => "Server is healthy");
 
 app.MapGet("games", () => games);
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id)).WithName(GetGameEndpointName);
+
+app.MapPost("games", (CreateGameDto newGame) =>
+{
+  Console.WriteLine(newGame);
+  GameDto game = new(
+    games.Count + 1,
+    newGame.Name,
+    newGame.Genre,
+    newGame.Price,
+    newGame.ReleaseDate
+  );
+
+  games.Add(game);
+
+  return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+
+
+});
 
 app.Run();
