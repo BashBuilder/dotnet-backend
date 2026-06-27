d2104b2f3e88f26fb6ffa53bf82a26de08e25791
Bashbuilder <iamadelowotan@gmail.com>
2026-06-27 12:02:56 +0100

    feat: added a get request

--- Program.cs
+++ Program.cs
@@ -1,6 +1,34 @@
+using backend.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = [
+  new(
+    1,
+    "Street Fighter II",
+    "Fighting",
+    19.99M,
+    new DateOnly(1992, 6, 12)
+  ),
+  new(
+    2,
+    "Final Fantasy",
+    "Role Playing",
+    49.99M,
+    new DateOnly(1992, 6, 10)
+  ),
+  new(
+    3,
+    "Fifa 23",
+    "Sports",
+    49.99M,
+    new DateOnly(2002, 1, 10)
+  ),
+];
+
+app.MapGet("/", () => "Hello World from a c# application");
+
+app.MapGet("games", () => games);
 
 app.Run();
